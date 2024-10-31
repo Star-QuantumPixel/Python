e4bd3e855a9a44d881dc1be8238230eb8272c43e
Star-QuantumPixel <aoun6755@gmail.com>
2024-10-30 22:11:05 -0700

    Add files via upload

---
+++ Sorting in list.py
@@ -0,0 +1,5 @@
+#sorting list in python
+list=["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","X","Z","X","C","V","B","N","M"]
+print("Ascending order:",sorted(list))
+print("\n")
+print("Descending order:",sorted(list,reverse=True))
